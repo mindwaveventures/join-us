64d389cd96433d8643c29d4bde94bbcffbbffa0b
elumalaidhinakaran1993 <88921164+elumalaidhinakaran1993@users.noreply.github.com>
2023-06-21 14:33:45 +0530

    added-assessment

--- assessment.js
+++ assessment.js
@@ -1,5 +1,5 @@
 
-
+// 1. Longest String
 const longeststring =(string)=>{
 
     const word = string.split(' ');
@@ -19,6 +19,10 @@ const longestWord = longeststring(inputString);
 
 console.log(longestWord);
 
+
+
+//2.repeatString
+
 const repeatString = (string,n)=>{
 return string.repeat(n)
 }
@@ -27,6 +31,42 @@ const repeatStrings = 'abc';
 const repeatStr = repeatString(repeatStrings,3)
 console.log(repeatStr);
 
+//3.remove duplicate 
 
+const removeDuplicates=(arr)=>{
+    return [...new Set(arr)]
+}
+
+const inputArray = [1,20,3,1,3,3];
+
+const uniqueArray = removeDuplicates(inputArray)
+
+console.log(uniqueArray);
+
+//4.Remove falsyvalue
+
+const removeFalsyValue =(arr)=>{
+    return arr.filter(Boolean)
+}
+
+const inputValues = [42,"everything","",2,false,"everything"]
+const filterValues = removeFalsyValue(inputValues);
+console.log(filterValues);
+
+
+//5.Trungate String
+
+const truncatString = (str,num)=>{
+
+    if(str.length <= num){
+        return str
+    }else{
+        return str.slice(0,num) + '...'
+    }
+}
+
+const inputStr = 'Abosolute victory';
+const truncateStrings = truncatString(inputStr,3);
+console.log(truncateStrings);
 
 
