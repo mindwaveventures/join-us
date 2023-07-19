3bffcac9e9afa637397cff159f2fd46020b87e98
Nivetha Nivi <nivethanivi0331@gmail.com>
2023-07-19 17:20:02 +0530

    Adding comments in the func

--- src/removeDuplicatesFromArray.js
+++ src/removeDuplicatesFromArray.js
@@ -1,6 +1,8 @@
+// Function for removing duplicates in an Array
 function removeDuplicates(array) {
   var uniqueElements = [];
 
+// Iterate the input array and check if the element already exisits
   for (var i = 0; i < array.length; i++) {
     if (uniqueElements.indexOf(array[i]) === -1) {
       uniqueElements.push(array[i]);
@@ -9,5 +11,3 @@ function removeDuplicates(array) {
   
   return uniqueElements;
 }
-
-
