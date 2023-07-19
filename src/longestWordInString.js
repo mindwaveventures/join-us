c6c2a5061b0ee7491d190b207e17a9e8c2d27996
Nivetha Nivi <nivethanivi0331@gmail.com>
2023-07-19 16:48:28 +0530

    included comments

--- src/longestWordInString.js
+++ src/longestWordInString.js
@@ -1,11 +1,17 @@
+//Function for finding the longest word in string
 function findLongestWordInString(str) {
+  // Splitting the string into array of words
   const words = str.split(" ");
 
+  // Variable Initialization
   let longestWord = "";
 
+  // Iterating each word in the array
   for (let i = 0; i < words.length; i++) {
+    // Removing any non-alphanumeric characters from the word
     const word = words[i].replace(/[^a-zA-Z0-9]/g, "");
 
+    // If the current word is longer than the previous longest word, update the longest word
     if (word.length > longestWord.length) {
       longestWord = word;
     }
