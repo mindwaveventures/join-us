30b29937664d74ba9ee9df38f9a9946f83c9795e
Nivetha Nivi <nivethanivi0331@gmail.com>
2023-07-19 18:17:54 +0530

    Repeat String N times with html

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+//Function to repeat the given string N times
+// function repeatStringNtimes(str, count) {
+//     var data = "";
+
+//     for (var each = 0; each<count;each++){
+//         data += str;
+//     }
+
+//     console.log(data)
+// }
+
+// var inputString = "abc";
+// var result = repeatStringNtimes(inputString, 3);
