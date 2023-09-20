b68cdf8c842e8aadc8bf62132e8079d768c34203
ses <ses@integra-india.com>
2023-09-20 18:06:29 +0530

    4 tasks are completed

--- task.js
+++ task.js
@@ -27,6 +27,7 @@ function repeatvalues(str, n) {
   console.log(rewritestring); 
 /* end Task */
 
+
 /* 
 Remove duplicates in an array
 If [1, 20, 3, 1, 3, 3] is the input to your function, it should return [1, 20, 3] 
@@ -34,18 +35,44 @@ If [1, 20, 3, 1, 3, 3] is the input to your function, it should return [1, 20, 3
 function deleteDuplicates(arr) {
     let uniArray = [];
     let valuejson = {};
-  
     for (let i = 0; i < arr.length; i++) {
       if (!seen[arr[i]]) {
         uniArray.push(arr[i]);
         valuejson[arr[i]] = true;
       }
     }
-  
     return uniqueArray;
   }
   let values = [1, 20, 3, 1, 3, 3];
-  let result = deleteDuplicates(values);
+  let results = deleteDuplicates(values);
   console.log(result);
+/* end Task */
 
+/*Remove falsy values
+If [42, "everything", "", 2, false, "everything"] is the input to your function, it should return [42, "everything", 2, "everything" 
+*/
+  function removeValues(arr) {
+    return arr.filter(Boolean);
+  }
+  let inputArray = [42, "everything", "", 2, false, "everything"];
+  let resulte = removeValues(inputArray);
+  console.log(result); 
+  /* end Task */
+
+
+  /*
+  Truncate a string
+  If 'Absolute victory' and 3 are the inputs to your function, it should return Abs...
+  */
+  function truncateString(str, maxLength) {
+    if (str.length <= maxLength) {
+      return str; 
+    } else {
+      return str.slice(0, maxLength) + '...'; 
+    }
+  }
+  let givenString = 'Absolute victory';
+  let maxLength = 3;
+  let truncated = truncateString(inputString, maxLength);
+  console.log(truncated); 
 /* end Task */
